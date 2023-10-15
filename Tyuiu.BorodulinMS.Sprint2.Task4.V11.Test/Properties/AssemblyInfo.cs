afc8d9330d55d2ecb9825a032e1a63f7e38880b9
Tyuiu.BorodulinMS@outlook.com <matweyborodulin@gmail.com>
2023-10-15 19:57:32 +0500

    Task4

---
+++ Tyuiu.BorodulinMS.Sprint2.Task4.V11.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.BorodulinMS.Sprint2.Task4.V11.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.BorodulinMS.Sprint2.Task4.V11.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("1334973d-faa4-4d78-81f7-ca27405b9bb1")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
